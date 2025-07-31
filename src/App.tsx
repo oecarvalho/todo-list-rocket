7b8cfe2c37b0f9a5a4e685c4b6956bc855945768
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-07-31 17:29:05 -0300

    Adicionando Badges

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import CloseIcon from './assets/icons/Close.svg?react'
 import PencilIcon from './assets/icons/Pencil.svg?react'
 import PlusIcon from './assets/icons/Plus.svg?react'
 import SpinnerIcon from './assets/icons/spinner.svg?react'
+import { Badge } from './components/Badge'
 
 
 
@@ -20,6 +21,8 @@ export default function App() {
     <>
       <Text variant={'body-md-bold'}>Olá Pessoal</Text>
       <Icon svg={TrashIcon} className='fill-pink-base'/>
+      <Badge variant={'primary'}>5</Badge>
+      <Badge variant={'secondary'}>2 de 5</Badge>
     </>
   )
 }
