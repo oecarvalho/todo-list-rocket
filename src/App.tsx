acd07a0dd3ef597cd6bed42b826088b68df817d0
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-07-31 00:05:37 -0300

    first Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Text } from './components/Text'
+
+export default function App() {
+
+  return (
+    <>
+      <Text variant={'body-md-bold'}>Olá Pessoal</Text>
+    </>
+  )
+}
+
